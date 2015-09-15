cbec7e292cfa8a0a3b392e10e62a4863ab304376
Chandan Kumar <chandan.bhardwaj22@gmail.com>
2015-09-15 16:46:19 +0100

    proxy setter

---
+++ src/main/java/test/skygo/Dummy.java
@@ -0,0 +1,14 @@
+package test.skygo;
+
+import org.junit.Test;
+
+/**
+ * Created by cku04 on 08/09/2015.
+ */
+public class Dummy {
+
+    @Test
+    public void doIt() throws Exception {
+        System.out.println("print message \n" + "next line");
+    }
+}
